54d3a1e84ad06b55efe652186227fbdb98c33fef
nusratamaisha <nusratamaisha@gmail.com>
2026-09-01 12:33:07 +0600

    feat: add User resource with email templates

---
+++ resources/user.py
@@ -0,0 +1,97 @@
+import os
+import requests
+from flask.views import MethodView
+from flask import current_app
+from flask_smorest import Blueprint, abort
+from passlib.hash import pbkdf2_sha256  #pbkdf2_sha256 used to hash password
+from flask_jwt_extended import (create_access_token, get_jwt, jwt_required,)
+from sqlalchemy import or_
+from db import db
+from models import UserModel
+from schemas import UserSchema, UserRegisterSchema
+from blocklist import BLOCKLIST
+from tasks import send_user_registration_email
+import redis
+from rq import Queue
+from tasks import send_user_registration_email
+
+
+
+blp = Blueprint("Users", "users", description="Operations on users")
+
+
+connection = redis.from_url(
+    os.getenv("REDIS_URL")
+)  # Get this from Render.com or run in Docker
+queue = Queue("emails", connection=connection)
+
+
+def send_simple_message(to, subject, body):
+    return requests.post(
+        "https://api.mailgun.net/v3/sandbox7a72e6a9375741a9bf51e4b8cc9e16ef.mailgun.org/messages",
+        auth=("api", os.getenv("MAILGUN_API_KEY")),
+        data={
+            "from": "Mailgun Sandbox <postmaster@sandbox7a72e6a9375741a9bf51e4b8cc9e16ef.mailgun.org>",
+            "to": to,
+            "subject": subject,
+            "text": body,
+        },
+    )
+
+@blp.route("/register")
+class UserRegister(MethodView):
+    @blp.arguments(UserRegisterSchema)
+    def post(self, user_data):
+        if UserModel.query.filter(
+            or_(
+                UserModel.username == user_data["username"],
+                UserModel.email == user_data["email"]
+                )
+        ).first():
+            abort(409, message="A user with that username already exists.")
+
+        user = UserModel(
+            username=user_data["username"],
+            email=user_data["email"],
+            password=pbkdf2_sha256.hash(user_data["password"]),
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        current_app.queue.enqueue(send_user_registration_email, user.email, user.username)
+        return {"message": "User created successfully."}, 201
+
+
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(UserModel.username == user_data["username"]).first()
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password):
+            access_token = create_access_token(identity=str(user.id))
+            return {"access_token": access_token}
+        abort(401, message="Invalid credentials.")
+
+
+@blp.route("/user/<int:user_id>")
+class User(MethodView):
+
+    @blp.response(200, UserSchema)
+    def get(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        return user
+
+    def delete(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        db.session.delete(user)
+        db.session.commit()
+        return {"message": "User deleted."}, 200
+
+
+@blp.route("/logout")
+class UserLogout(MethodView):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        return {"message": "Successfully logged out"}, 200
