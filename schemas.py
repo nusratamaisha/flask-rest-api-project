100d3666ab1a3dda9286c3559676fd09fc9ba39b
nusratamaisha <nusratamaisha@gmail.com>
2026-09-01 12:13:02 +0600

    feat: add Marshmallow schemas

---
+++ schemas.py
@@ -0,0 +1,60 @@
+from marshmallow import Schema, fields
+
+
+#load_only -> passwords that are sent but can't be sent back
+#dump_only -> where user can not manipulate any data
+
+class PlainItemSchema(Schema):
+    id = fields.Int(dump_only=True) #dump means serialize and load means deserialize
+    name = fields.Str(required=True)
+    price = fields.Float(required=True)
+
+
+class PlainStoreSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
+
+class ItemUpdateSchema(Schema):
+    name = fields.Str()
+    price = fields.Float()
+
+
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
+
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+
+
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+
+
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    username = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
+
+
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
+
+class UserRegisterSchema(UserSchema):
+    email = fields.Email(required=True)
